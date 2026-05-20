fb79fab31b73e380abb927b0ed781b2796b57054
Aliqyan-21 <aliqyanabid21@gmail.com>
2026-05-20 15:58:05 +0530

    feat: lexer number, ident, one char tokens

--- include/lx4c.c
+++ include/lx4c.c
@@ -2,6 +2,7 @@
 #include <ctype.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 
 typedef enum lx4c_token_type {
   TOK_IDENT,  /* single letter only: x,y, E */
@@ -47,6 +48,20 @@ static void lexer_init(lx4c_lexer *lex, const char *src, size_t len) {
   lex->pos = 0;
 }
 
+/* readers */
+inline static void read_cmd(lx4c_lexer *lex) {
+  while (lex->pos < lex->len && isalpha((unsigned char)lex->src[lex->pos])) {
+    lex->pos++;
+  }
+}
+
+inline static void read_number(lx4c_lexer *lex) {
+  while (lex->pos < lex->len && (isdigit((unsigned char)lex->src[lex->pos]) ||
+                                 lex->src[lex->pos] == '.')) {
+    lex->pos++;
+  }
+}
+
 static lx4c_token tokenize_next(lx4c_lexer *lex) {
   lx4c_token tok;
   skip_whitespace(lex);
@@ -66,22 +81,61 @@ static lx4c_token tokenize_next(lx4c_lexer *lex) {
   if (c == '\\') {
     lex->pos++;
     size_t cmd_start = lex->pos;
-    while (lex->pos < lex->len && isalpha((unsigned char)lex->src[lex->pos])) {
-      lex->pos++;
-    }
+    read_cmd(lex);
     tok.type  = TOK_CMD;
     tok.start = lex->src + cmd_start;
     tok.len   = lex->pos - cmd_start;
     return tok;
   }
 
+  /* number: 42, 3.14 */
+  if (isdigit((unsigned char)c)) {
+    read_number(lex);
+    tok.type = TOK_NUMBER;
+    tok.len  = lex->pos - (tok.start - lex->src);
+    return tok;
+  }
+
+  /* ident */
+  if (isalpha((unsigned char)c)) {
+    lex->pos++;
+    tok.type = TOK_IDENT;
+    tok.len  = 1;
+    return tok;
+  }
+
+  /* single char tokens */
+  lex->pos++;
+  tok.len = 1;
+  switch (c) {
+    case '{': tok.type = TOK_LBRACE; break;
+    case '}': tok.type = TOK_RBRACE; break;
+    case '[': tok.type = TOK_LBRACKET; break;
+    case ']': tok.type = TOK_RBRACKET; break;
+    case '(': tok.type = TOK_LPAREN; break;
+    case ')': tok.type = TOK_RPAREN; break;
+    case '^': tok.type = TOK_SUP; break;
+    case '_': tok.type = TOK_SUB; break;
+    default: tok.type = TOK_OTHER; break;
+  }
+
   return tok;
 }
 
 lx4c_node *lx4c_parse(const char *latex, size_t len) {
-  lx4c_lexer lex;
-  lexer_init(&lex, "\\frac", 5);
-  lx4c_token tok = tokenize_next(&lex);
-  printf("%zu\n", tok.len);
-  printf("%s\n", tok.start);
+  const char *test = "\\frac{x^{2}}{\\alpha + 3.14}";
+  lx4c_lexer  lex;
+  lexer_init(&lex, test, strlen(test));
+
+  static const char *tok_names[] = {
+    "IDENT",  "NUMBER", "CMD", "LBRACE", "RBRACE", "LBRACKET", "RBRACKET",
+    "LPAREN", "RPAREN", "SUP", "SUB",    "OTHER",  "EOF"};
+
+  lx4c_token tok;
+  do {
+    tok = tokenize_next(&lex);
+    printf("%-10s | %.*s\n", tok_names[tok.type], (int)tok.len, tok.start);
+  } while (tok.type != TOK_EOF);
+
+  return NULL;
 }

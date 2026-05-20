407d5d81d264abe52fdab3141765cc08b1d71faf
Aliqyan-21 <aliqyanabid21@gmail.com>
2026-05-20 11:52:59 +0530

    feat: lx4c nodes and tokens
    what we are going to support for this version

--- include/lx4c.c
+++ include/lx4c.c
@@ -1,1 +1,25 @@
 #include "lx4c.h"
+#include <ctype.h>
+#include <stdbool.h>
+
+typedef enum lx4c_token_type {
+  TOK_IDENT,  /* single letter only: x,y, E */
+  TOK_NUMBER, /* digits */
+  TOK_CMD,    /* backlash command: \frac, \sqrt, \beta, etc. */
+  TOK_LBRACE,
+  TOK_RBRACE,
+  TOK_LBRACKET,
+  TOK_RBRACKET,
+  TOK_LPAREN,
+  TOK_RPAREN,
+  TOK_SUP,   /* ^ */
+  TOK_SUB,   /* _ */
+  TOK_OTHER, /* any unrecognized token */
+  TOK_EOF,
+} lx4c_token_type;
+
+typedef struct {
+  lx4c_token_type type;
+  const char     *start;
+  size_t          len;
+} lx4c_token;
--- include/lx4c.h
+++ include/lx4c.h
@@ -1,6 +1,61 @@
 #ifndef LX4C_H
 #define LX4C_H
 
+#include <stddef.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
 
+
+typedef enum lx4c_node_type {
+  /* leaf nodes */
+  LX4C_NODE_IDENT,  /*  x,y,E */
+  LX4C_NODE_NUMBER, /*  42, 3.14 */
+  LX4C_NODE_OP,     /*  +, =, \leq, \times */
+  LX4C_NODE_TEXT,   /* \text{hello} */
+
+  /* strcture nodes */
+  LX4C_NODE_ROW,    /* seq of nodes */
+  LX4C_NODE_FRAC,   /* \frac{a}{b} */
+  LX4C_NODE_SQRT,   /* \sqrt{x} */
+  LX4C_NODE_ROOT,   /* \sqrt[n]{x} */
+  LX4C_NODE_SUP,    /* x^{n} */
+  LX4C_NODE_SUB,    /* x_{i} */
+  LX4C_NODE_SUBSUP, /* x_{i}^{n} */
+  LX4C_NODE_OVER,   /* \overline, \vec */
+  LX4C_NODE_UNDER,  /* \underline */
+
+  LX4C_NODE_UNKNOWN, /* unrecognized token */
+} lx4c_node_type;
+
+typedef struct lx4c_node lx4c_node;
+typedef struct lx4c_node {
+  lx4c_node_type type;
+
+  /* leaf value is valid for IDENT, NUMBER, OP, TEXT, UNKNOWN */
+  const char *value; /* original input string pointer */
+  size_t      value_len;
+
+  /* for OP/OVER/UNDER */
+  const char *symbol; /* e.g. "\leq" -> "<=", NULL if unused */
+
+  /* children -> for valid structural nodes */
+  lx4c_node **children;
+  int         child_count;
+} lx4c_node;
+
+/* Parse a latex math string, args:
+ * - latex : input string, does not neet to be null-terminated
+ * - len : length of input
+ * - returns - root node, or NULL on allocation failure and others(?)
+ */
+lx4c_node *lx4c_parse(const char *latex, size_t len);
+
+/* free the full AST returned by the above function */
+void lx4c_free(lx4c_node *root);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif  //! LX4C_H
